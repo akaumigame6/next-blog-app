6331b474c29b8df0deec81d5d6a3c30383bc3bc2
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2024-12-12 12:59:48 +0900

    ８回目まで

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { twMerge } from "tailwind-merge";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFish } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="bg-slate-800 py-2 font-bold text-white">
+        <div
+          className={twMerge(
+            "mx-4 max-w-2xl md:mx-auto",
+            "flex items-center justify-between",
+            "text-lg font-bold text-white"
+          )}
+        >
+          <div>
+            <FontAwesomeIcon icon={faFish} className="mr-1" />
+            I&apos;Mのブログ
+          </div>
+          <div>
+            <Link href="/">
+              <FontAwesomeIcon icon={faFish} className="mr-1" />
+              Header
+            </Link>
+          </div>
+          <div>
+            <Link href="/about">About</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
