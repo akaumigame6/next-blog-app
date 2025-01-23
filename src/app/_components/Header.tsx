72bcde597e42584d592732315362ab8f5d4da744
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2025-01-23 09:09:29 +0900

    ルートガード実装まで

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -2,9 +2,19 @@
 import { twMerge } from "tailwind-merge";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFish } from "@fortawesome/free-solid-svg-icons";
+import { supabase } from "@/utils/supabase"; // ◀ 追加
+import { useAuth } from "@/app/_hooks/useAuth"; // ◀ 追加
+import { useRouter } from "next/navigation"; // ◀ 追加
 import Link from "next/link";
 
 const Header: React.FC = () => {
+  const router = useRouter();
+  const { isLoading, session } = useAuth();
+  const logout = async () => {
+    await supabase.auth.signOut();
+    router.replace("/");
+  };
+
   return (
     <header>
       <div className="bg-teal-500 py-2 font-bold text-white">
@@ -26,6 +36,16 @@ const Header: React.FC = () => {
             </Link>
           </div>
           <div>
+            {!isLoading &&
+              (session ? (
+                <button onClick={logout} className="mx-3">
+                  Logout{" "}
+                </button>
+              ) : (
+                <Link href="/login" className="mx-3">
+                  Login
+                </Link>
+              ))}
             <Link href="/about">
               <FontAwesomeIcon icon={faFish} className="mr-1" />
               About
